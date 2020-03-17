86c5cd45c3a3d7fc9d5dcb4d81cb70e415ca25ec
fxduportal <fxduportal@outlook.fr>
2020-03-17 08:27:27 +0000

    Step 2,bib in json

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,13 +1,21 @@
 /* eslint-disable no-console, no-process-exit */
 const michelin = require('./michelin');
 
-async function sandbox(searchLink = "https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/"){
+async function sandbox(searchLink = "https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/page/") {
     try {
         console.log(`🕵️‍♀️  browsing ${searchLink} source`);
-        
+
         const restaurant = await michelin.get(searchLink);
 
-        console.log(restaurant);
+        var fs = require('fs');
+        fs.writeFileSync('server/bibList.json', JSON.stringify(restaurant, null, 4), (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            };
+            console.log("File filled");
+        });
+
         console.log('done');
         process.exit(0);
     } catch (e) {
